de0d5c25a76330d206f75736b1c3831f8e66a6eb
tonplamm <tonplamm@Kanokporns-MacBook-Air.local>
2017-09-26 17:01:03 +0700

    bug cannot delete both content1 and content4

---
+++ public/javascripts/index.js
@@ -0,0 +1,101 @@
+function bindDeleteNews(news) {
+  $('#content1 .btn.delete').click(function() {
+    var item_id = $(this).closest('.row').attr('item_id');
+    $.post('/api/deletenews', {_id: item_id}, function(data){
+      console.log(data);
+      getNews(news);
+    });
+
+  });
+}
+
+function getNews(news) {
+  $.get('/api/shownews', function(data){
+    news.find('.row:not(.header)').remove();
+    $.each(data, function(i, item) {
+      console.log(item)
+      news.append('<div class="row" item_id="' + item._id + '"><div class="cell">'+item.name+'</div><div class="cell"><button class="btn link"><a href="'+item.link+'" target="_blank">click</a></button></div>' + (admin_session ? '<div class="cell btn delete" id="admin">X</div>' : '') + '</div>');
+    });
+  });
+  bindDeleteNews(news);
+}
+
+function bindDeletePlaces(places) {
+  $('#content4 .btn.delete').click(function() {
+    var item_id = $(this).closest('.row').attr('item_id');
+    $.post('/api/deleteplaces', {_id: item_id}, function(data){
+      console.log(data);
+      getPlaces(places);
+    });
+
+  });
+}
+
+function getPlaces(places) {
+  $.get('/api/showplaces', function(data){
+    places.find('.row:not(.header)').remove();
+    $.each(data, function(i, item) {
+      console.log(item)
+      places.append('<div class="row" item_id="' + item._id + '"><div class="cell">'+item.name+'</div><div class="cell"><button class="btn link"><a href="'+item.link+'" target="_blank">click</a></button></div>' + (admin_session ? '<div class="cell btn delete" id="admin">X</div>' : '') + '</div>');
+    });
+  });
+  bindDeletePlaces(places);
+}
+
+$(document).ready(function(){
+  var addnewsYear = $('#addnewsYear');
+  var addnewsName = $('#addnewsName');
+  var addnewsLink = $('#addnewsLink');
+  var news = $('#news');
+  $('#addnewsButton').click(function(){
+    $.post('/api/addnews',{name: addnewsName.val(), link: addnewsLink.val(), year: addnewsYear.val()}, function(data) {
+      console.log(data);
+      if(data.ok === 1) {
+        addnewsYear.val("");
+        addnewsName.val("");
+        addnewsLink.val("");
+      }
+    });
+  })
+  getNews(news);
+
+  var addplacesYear = $('#addplacesYear');
+  var addplacesName = $('#addplacesName');
+  var addplacesLink = $('#addplacesLink');
+  var places = $('#places');
+  $('#addplacesButton').click(function(){
+    $.post('/api/addplaces',{name: addplacesName.val(), link: addplacesLink.val(), year: addplacesYear.val()}, function(data) {
+      console.log(data);
+      if(data.ok === 1) {
+        addplacesYear.val("");
+        addplacesName.val("");
+        addplacesLink.val("");
+      }
+    });
+  })
+  getPlaces(places);
+
+  // var filecpe = $('#file-cpe');
+  // var addcpeYear = $('#addcpeYear');
+  // var addcpeName = $('#addcpeName');
+  // var cpe = $('#cpe');
+  //
+  // $('#addcpeYear').onkeyup(function(){
+  //   console.log('jer');
+  // });
+  //
+  //
+  // document.getElementById('file-cpe').onchange = function(){
+  //   var file = this.files[0];
+  //   var reader = new FileReader();
+  //   reader.onload = function(progressEvent){
+  //     var lines = this.result.split('\n');
+  //     for(var line = 0; line < lines.length; line++){
+  //       var check = lines[line].split(",");
+  //       console.log(check[0]+check[1]);
+  //     }
+  //   };
+  //   reader.readAsText(file);
+  // };
+
+});
--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var monk = require('monk');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -20,4 +21,69 @@ router.post('/login', function(req, res, next) {
   });
 });
 
+/* news */
+router.post('/deletenews', function(req, res, next){
+    req.db.get('news').remove({_id: monk.id(req.body._id)}).then(docs => {
+      res.send(docs);
+    });
+});
+
+router.get('/shownews', function(req, res, next){
+  req.db.get('news').find({}).then(docs => {
+    res.send(docs);
+  });
+});
+
+router.post('/addnews', function(req, res, next){
+  if(req.session.admin && parseInt(req.body.year)) {
+    req.db.get('news').find({name: req.body.name}).then(docs => {
+      if(docs.length === 0) {
+        req.db.get('news').insert({name: req.body.name, link: req.body.link, year: parseInt(req.body.year), date: new Date()}).then(docs => {
+          if(docs) {
+            res.send({ok: 1});
+          } else {
+            res.send({ok: 0});
+          }
+        })
+      } else {
+        res.send({ok: 0});
+      }
+    });
+  } else {
+    res.send({ok: 0})
+  }
+});
+
+/*places*/
+router.post('/deleteplaces', function(req, res, next){
+    req.db.get('places').remove({_id: monk.id(req.body._id)}).then(docs => {
+      res.send(docs);
+    });
+});
+
+router.get('/showplaces', function(req, res, next){
+  req.db.get('places').find({}).then(docs => {
+    res.send(docs);
+  });
+});
+
+router.post('/addplaces', function(req, res, next){
+  if(req.session.admin && parseInt(req.body.year)) {
+    req.db.get('places').find({name: req.body.name}).then(docs => {
+      if(docs.length === 0) {
+        req.db.get('places').insert({name: req.body.name, link: req.body.link, year: parseInt(req.body.year), date: new Date()}).then(docs => {
+          if(docs) {
+            res.send({ok: 1});
+          } else {
+            res.send({ok: 0});
+          }
+        })
+      } else {
+        res.send({ok: 0});
+      }
+    });
+  } else {
+    res.send({ok: 0})
+  }
+});
 module.exports = router;
