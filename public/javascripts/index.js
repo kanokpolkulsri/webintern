b0554af40727139c5a788b775820f9c8df0b3f48
tonplamm <tonplam_kanokpol@hotmail.com>
2017-10-09 19:03:01 +0700

    can add and edit content1 and content4, can read file upload both of cpe and ske

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,7 @@
 function bindDeleteNews(news) {
   $('#content1 .btn.delete').click(function() {
-    console.log('jerr');
     var item_id = $(this).closest('.row').attr('item_id');
-    console.log(item_id);
     $.post('/api/deletenews', {_id: item_id}, function(data){
-      // console.log(data);
       getNews(news);
     });
 
@@ -18,31 +15,31 @@ function getNews(news) {
       console.log(item)
       news.append('<div class="row" item_id="' + item._id + '"><div class="cell">'+item.name+'</div><div class="cell"><button class="btn link"><a href="'+item.link+'" target="_blank">click</a></button></div>' + (admin_session ? '<div class="cell btn delete" id="admin">X</div>' : '') + '</div>');
     });
+    bindDeleteNews(news);
   });
-  bindDeleteNews(news);
 }
 
-// function bindDeletePlaces(places) {
-//   $('#content4 .btn.delete').click(function() {
-//     var item_id = $(this).closest('.row').attr('item_id');
-//     $.post('/api/deleteplaces', {_id: item_id}, function(data){
-//       console.log(data);
-//       getPlaces(places);
-//     });
-//
-//   });
-// }
-//
-// function getPlaces(places) {
-//   $.get('/api/showplaces', function(data){
-//     places.find('.row:not(.header)').remove();
-//     $.each(data, function(i, item) {
-//       console.log(item)
-//       places.append('<div class="row" item_id="' + item._id + '"><div class="cell">'+item.name+'</div><div class="cell"><button class="btn link"><a href="'+item.link+'" target="_blank">click</a></button></div>' + (admin_session ? '<div class="cell btn delete" id="admin">X</div>' : '') + '</div>');
-//     });
-//   });
-//   bindDeletePlaces(places);
-// }
+function bindDeletePlaces(places) {
+  $('#content4 .btn.delete').click(function() {
+    var item_id = $(this).closest('.row').attr('item_id');
+    $.post('/api/deleteplaces', {_id: item_id}, function(data){
+      console.log(data);
+      getPlaces(places);
+    });
+
+  });
+}
+
+function getPlaces(places) {
+  $.get('/api/showplaces', function(data){
+    places.find('.row:not(.header)').remove();
+    $.each(data, function(i, item) {
+      console.log(item)
+      places.append('<div class="row" item_id="' + item._id + '"><div class="cell">'+item.name+'</div><div class="cell"><button class="btn link"><a href="'+item.link+'" target="_blank">click</a></button></div>' + (admin_session ? '<div class="cell btn delete" id="admin">X</div>' : '') + '</div>');
+    });
+    bindDeletePlaces(places);
+  });
+}
 
 $(document).ready(function(){
   var addnewsYear = $('#addnewsYear');
@@ -61,47 +58,59 @@ $(document).ready(function(){
     });
   })
   getNews(news);
-  $('#content1 .cell.btn.delete').click(function() {
-    console.log('jerr');
-  });
 
-  // var addplacesYear = $('#addplacesYear');
-  // var addplacesName = $('#addplacesName');
-  // var addplacesLink = $('#addplacesLink');
-  // var places = $('#places');
-  // $('#addplacesButton').click(function(){
-  //   $.post('/api/addplaces',{name: addplacesName.val(), link: addplacesLink.val(), year: addplacesYear.val()}, function(data) {
-  //     console.log(data);
-  //     if(data.ok === 1) {
-  //       addplacesYear.val("");
-  //       addplacesName.val("");
-  //       addplacesLink.val("");
-  //     }
-  //   });
-  // })
-  // getPlaces(places);
+  var addplacesYear = $('#addplacesYear');
+  var addplacesName = $('#addplacesName');
+  var addplacesLink = $('#addplacesLink');
+  var places = $('#places');
+  $('#addplacesButton').click(function(){
+    $.post('/api/addplaces',{name: addplacesName.val(), link: addplacesLink.val(), year: addplacesYear.val()}, function(data) {
+      console.log(data);
+      if(data.ok === 1) {
+        addplacesYear.val("");
+        addplacesName.val("");
+        addplacesLink.val("");
+      }
+    });
+  })
+  getPlaces(places);
 
-  // var filecpe = $('#file-cpe');
-  // var addcpeYear = $('#addcpeYear');
-  // var addcpeName = $('#addcpeName');
-  // var cpe = $('#cpe');
-  //
-  // $('#addcpeYear').onkeyup(function(){
-  //   console.log('jer');
-  // });
-  //
-  //
-  // document.getElementById('file-cpe').onchange = function(){
-  //   var file = this.files[0];
-  //   var reader = new FileReader();
-  //   reader.onload = function(progressEvent){
-  //     var lines = this.result.split('\n');
-  //     for(var line = 0; line < lines.length; line++){
-  //       var check = lines[line].split(",");
-  //       console.log(check[0]+check[1]);
-  //     }
-  //   };
-  //   reader.readAsText(file);
-  // };
+  var filecpe = $('#file-cpe');
+  var addcpeYear = $('#addcpeYear');
+  var addcpeName = $('#addcpeName');
+  var cpe = $('#cpe');
+
+  document.getElementById('file-cpe').onchange = function(){
+    var file = this.files[0];
+    var reader = new FileReader();
+    reader.onload = function(progressEvent){
+      var lines = this.result.split('\n');
+      for(var line = 0; line < lines.length; line++){
+        var check = lines[line].split(",");
+        console.log(check[0]+check[1]);
+      }
+    };
+    reader.readAsText(file);
+  };
+
+  var fileske = $('#file-ske');
+  var addskeYear = $('#addskeYear');
+  var addskeName = $('#addskeName');
+  var ske = $('#ske');
+
+  document.getElementById('file-ske').onchange = function(){
+    var file = this.files[0];
+    var reader = new FileReader();
+    reader.onload = function(progressEvent){
+      var lines = this.result.split('\n');
+      for(var line = 0; line < lines.length; line++){
+        var check = lines[line].split(",");
+        if(check.length < 5)
+          break;
+        console.log(check[0]+check[1]);
+      }
+    };
+    reader.readAsText(file);
+  };
 
 });
